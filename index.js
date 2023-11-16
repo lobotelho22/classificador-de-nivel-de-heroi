b3198c8129b22344053fdf9f538c5547bdc45b7e
lobotelho22 <eduardo.ocara@gmail.com>
2023-11-16 19:21:04 -0300

    ref: organizando variavel global NIVEIS

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const { question, questionInt } = require('readline-sync')
+const { question, questionInt } = require('readline-sync');
+const { NIVEIS } = require('./src/globals');
 
 function obterNivel(XpHeroi) {
-    const niveis = [
-        "Ferro", "Bronze", "Prata", "Ouro", "Platina",
-        "Ascendente", "Imortal", "Radiante"
-    ]
 
     if (XpHeroi < 1000) {
-        return niveis[0];
+        return NIVEIS[0];
     } else if (XpHeroi < 2000) {
-        return niveis[1];
+        return NIVEIS[1];
     } else if (XpHeroi < 5000) {
-        return niveis[2];
+        return NIVEIS[2];
     } else if (XpHeroi < 7000) {
-        return niveis[3];
+        return NIVEIS[3];
     } else if (XpHeroi < 8000) {
-        return niveis[4];
+        return NIVEIS[4];
     } else if (XpHeroi < 9000) {
-        return niveis[5];
+        return NIVEIS[5];
     } else if (XpHeroi < 10000) {
-        return niveis[6];
+        return NIVEIS[6];
     }
 
-    return niveis[7]
+    return NIVEIS[7]
 }
 
 function main() {
