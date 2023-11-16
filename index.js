397c070bee381e43bf8f5d523d56b449fdc502b2
lobotelho22 <eduardo.ocara@gmail.com>
2023-11-16 18:58:17 -0300

    implementado o codigo

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const { question } = require('readline-sync')
+const { question, questionInt } = require('readline-sync')
 
-function main () {
-    console.log('teste');
-    const nomeDoHeroi = question('Digite o nome do herói: ');
-    console.log(nomeDoHeroi);
+function obterNivel(XpHeroi) {
+    const niveis = [
+        "Ferro", "Bronze", "Prata", "Ouro", "Platina",
+        "Ascendente", "Imortal", "Radiante"
+    ]
+
+    if (XpHeroi < 1000) {
+        return niveis[0];
+    } else if (XpHeroi < 2000) {
+        return niveis[1];
+    } else if (XpHeroi < 5000) {
+        return niveis[2];
+    } else if (XpHeroi < 7000) {
+        return niveis[3];
+    } else if (XpHeroi < 8000) {
+        return niveis[4];
+    } else if (XpHeroi < 9000) {
+        return niveis[5];
+    } else if (XpHeroi < 10000) {
+        return niveis[6];
+    }
+
+    return niveis[7]
+}
+
+function main() {
+    const nomeHeroi = question('Digite o nome do herói: ');
+    const heroiXP = questionInt('Informe a experiência do herói: ');
+
+    let nivelHeroi = obterNivel(heroiXP);
+
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
 }
 
 main();
